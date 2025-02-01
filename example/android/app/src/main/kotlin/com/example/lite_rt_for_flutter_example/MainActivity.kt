8805a6993a1e11bb24d1d7d372dfde082566f0ef
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-01 16:34:56 +0900

    init

---
+++ example/android/app/src/main/kotlin/com/example/lite_rt_for_flutter_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lite_rt_for_flutter_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Classes/lite_rt_for_flutter.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../lite_rt_for_flutter.podspec for more information.
+#include "../../src/lite_rt_for_flutter.c"
