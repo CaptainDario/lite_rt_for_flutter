9d1eb56a4196998efc893122fc16f6963a1dc20d
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-07 12:53:38 +0900

    updated lib loading for android, ios and macos

--- lib/libs.dart
+++ lib/libs.dart
@@ -3,26 +3,28 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 
+import 'package:lite_rt_for_dart/src/bindings/bindings.dart';
+import 'package:lite_rt_for_dart/src/bindings/tensorflow_lite_bindings_generated.dart';
+
 
 
 /// Get the `DynamicLibrary` of this plugin, ie. the base tf lite runtime
 String getLiteRTBaseLibraryPath(){
 
-  String libTfLitePath = "libtensorflowlite_c";
+  String libTfLitePath = "libtensorflowlite";
 
   if (Platform.isAndroid) {
-    // TODO
-    //tfLiteLib = DynamicLibrary.open('libtensorflowlite_jni.so');
+    libTfLitePath = '${libTfLitePath}_jni.so';
   }
   else if (Platform.isIOS) {
-    // TODO
-    //tfLiteLib = DynamicLibrary.process();
+    libTfLitePath = shouldUseDynamicLibraryProcess;
   }
   else if (Platform.isMacOS) {
     //if(Abi.current() == Abi.macosArm64) libName += "arm64";
     //else if(Abi.current() == Abi.macosArm64) libName += "x86";
     
-    libTfLitePath = "$libTfLitePath.dylib";
+    libTfLitePath = "${libTfLitePath}_c.dylib";
+    print(libTfLitePath);
   }
   else if (Platform.isLinux) {
     // TODO
