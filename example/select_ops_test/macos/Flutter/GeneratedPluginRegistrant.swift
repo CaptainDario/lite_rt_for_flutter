c4eb4deb56c7884fba9ded95c85205aaf4c0e193
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-05-05 20:31:23 +0200

    select ops

--- example/select_ops_test/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/select_ops_test/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import lite_rt_for_flutter_libs_flex_delegate_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LiteRtForFlutterLibsFlexDelegateMacosPlugin.register(with: registry.registrar(forPlugin: "LiteRtForFlutterLibsFlexDelegateMacosPlugin"))
 }
